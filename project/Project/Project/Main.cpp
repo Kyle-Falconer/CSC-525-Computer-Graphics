e59bc39932e6a07c3b01ff509372dbf312dfb2d9
unknown <garbageelitist@gmail.com>
2013-11-14 15:44:17 -0600

    Got the general layout working together, with a main window and three subwindows. I wasn't sure how far to go, since I'm in the territory now where changes should be pulling information from the finite state machine.

--- project/Project/Project/Main.cpp
+++ project/Project/Project/Main.cpp
@@ -26,6 +26,11 @@ using namespace std;
 
 #define KEY_ESCAPE 27
 
+int mainWindow;
+int viewWindow;
+int bottomWindow;
+int mapWindow;
+
 typedef struct {
 	int width;
 	int height;
@@ -37,19 +42,40 @@ typedef struct {
 } glutWindow;
 
 glutWindow win;
+glutWindow view;
+glutWindow bottom;
+glutWindow bottomMap;
 
 float g_rotation = 0;
 float g_rotation_speed = 0.2f;
 
-void display() 
+void drawWindowBox(glutWindow cur) {
+	glLineWidth(4);
+	glBegin(GL_LINE_STRIP);
+		glColor3f(1, 1, 1);
+		glVertex2f((cur.width / 2) - 8, (cur.height / 2) - 8);
+		glVertex2f((cur.width / 2) - 8, -(cur.height / 2) + 8);
+		glVertex2f(-(cur.width / 2) + 8, -(cur.height / 2) + 8);
+		glVertex2f(-(cur.width / 2) + 8, (cur.height / 2) - 8);
+		glVertex2f((cur.width / 2) - 8, (cur.height / 2) - 8);
+	glEnd();
+}
+
+void mainDisplay() {
+	glutSetWindow(mainWindow);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void viewDisplay() 
 {
+	glutSetWindow(viewWindow);
 	// Clear Screen and Depth Buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		     
 	glLoadIdentity();
 
 	// Define a viewing transformation
 	gluLookAt( 4,2,0, 0,0,0, 0,1,0);					  
-
 
 	// Push and pop the current matrix stack. 
 	// This causes that translations and rotations on this matrix wont influence others.
@@ -77,6 +103,45 @@ void display()
 	glutSwapBuffers();
 }
 
+void drawText(int win, float x, float y, float r, float g, float b, float scale, std::string text){
+	glutSetWindow(win);
+	glColor3f(1.0, 1.0, 1.0);
+	glRasterPos2i(x,y);
+	void * font = 	GLUT_BITMAP_9_BY_15;
+	for (string::iterator i = text.begin(); i != text.end(); ++i)
+	{
+		char c = *i;
+		glutBitmapCharacter(font, c);
+	}
+}
+
+void bottomDisplay() {
+	glutSetWindow(bottomWindow);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	drawWindowBox(bottom);
+
+	drawText(bottomWindow, -bottom.width / 2 + 50, bottom.height / 2 - 70, 1, 1, 1, 2,  "1 - First Option");
+	drawText(bottomWindow, -bottom.width / 2 + 50, -bottom.height / 2 + 70, 1, 1, 1, 2,  "2 - Second Option");
+
+	glutSwapBuffers();
+}
+
+void mapDisplay() {
+	glutSetWindow(mapWindow);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLineWidth(4);
+	
+	drawWindowBox(bottomMap);
+
+	glutSwapBuffers();
+}
+
+void displayCallback() {
+	viewDisplay();
+	bottomDisplay();
+	mapDisplay();
+}
 
 void initialize () 
 {
@@ -84,17 +149,17 @@ void initialize ()
 	glMatrixMode(GL_PROJECTION);												
 
 	// set the viewport
-	glViewport(0, 0, win.width, win.height);									
+	glViewport(0, 0, view.width, view.height);									
 
 	// set matrix mode
 	glMatrixMode(GL_PROJECTION);												
 
 	// reset projection matrix
 	glLoadIdentity();															
-	GLfloat aspect = (GLfloat) win.width / win.height;
+	GLfloat aspect = (GLfloat) view.width / (view.height);
 
 	// set up a perspective projection matrix
-	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);		
+	gluPerspective(view.field_of_view_angle, aspect, view.z_near, view.z_far);		
 
 	// specify which matrix is the current matrix
 	glMatrixMode(GL_MODELVIEW);													
@@ -122,42 +187,79 @@ void initialize ()
 	glEnable( GL_DEPTH_TEST );
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0); 
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearColor(1, 1, 1, 0);
 }
-
 
 void keyboard ( unsigned char key, int mousePositionX, int mousePositionY )		
 { 
 	switch ( key ) 
 	{
-	case KEY_ESCAPE:        
-		exit ( 0 );   
+	case KEY_ESCAPE:    
+		exit(0);   
 		break;      
 
 	default:      
 		break;
 	}
 }
+void mapInit() {
+	bottomMap.width = 256;
+	bottomMap.height = bottom.height;
+	mapWindow = glutCreateSubWindow(mainWindow, bottom.width, view.height, bottomMap.width, bottomMap.height);
+	glutDisplayFunc(mapDisplay);
+	glClearColor(0, 0, 0, 1);
+	glutKeyboardFunc(keyboard);						// register Keyboard Handler
+	gluOrtho2D(-bottomMap.width / 2, bottomMap.width / 2, -bottomMap.height / 2, bottomMap.height / 2);  // specify a viewing area
+}
 
-int main(int argc, char **argv) 
-{
+void bottomInit() {
 	// set window values
-	win.width = 640;
-	win.height = 480;
-	win.title = "OpenGL/GLUT Example. Visit http://openglsamples.sf.net ";
-	win.field_of_view_angle = 45;
-	win.z_near = 1.0f;
-	win.z_far = 500.0f;
+	bottom.width = 768;
+	bottom.height = 192;
 
+	bottomWindow = glutCreateSubWindow(mainWindow, 0, view.height, bottom.width, bottom.height);
+	glutDisplayFunc(bottomDisplay);
+	glutKeyboardFunc(keyboard);						// register Keyboard Handler
+	glClearColor(0, 0, 0, 1);			// specify a background
+	gluOrtho2D(-bottom.width / 2, bottom.width / 2, -bottom.height / 2, bottom.height / 2);  // specify a viewing area
+}
+
+void viewInit() {
+	// set window values
+	view.width = 1024;
+	view.height = 576;
+	view.field_of_view_angle = 45;
+	view.z_near = 1.0f;
+	view.z_far = 500.0f;
+
+	viewWindow = glutCreateSubWindow(mainWindow, 0, 0, view.width, view.height);		// create Window
+	glutDisplayFunc(viewDisplay);							// register Display Function
+	glutKeyboardFunc(keyboard);						// register Keyboard Handler
+	initialize();
+}
+
+void mainInit() {
+	// set window values
+	win.width = 1024;
+	win.height = 768;
+	win.title = "Finite State Machine Game (working title)";
+
+	glutInitWindowSize(win.width, win.height);
+	mainWindow = glutCreateWindow(win.title);
+	glutDisplayFunc(mainDisplay);
+	glutIdleFunc(displayCallback);					// register Idle Function
+	glutKeyboardFunc(keyboard);						// register Keyboard Handler
+	glClearColor(0, 0, 0, 0);
+}
+
+void main(int argc, char **argv) 
+{
 	// initialize and run program
 	glutInit(&argc, argv);                                      // GLUT initialization
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );  // Display Mode
-	glutInitWindowSize(win.width,win.height);				// set window size
-	glutCreateWindow(win.title);					// create Window
-	glutDisplayFunc(display);						// register Display Function
-	glutIdleFunc( display );						// register Idle Function
-	glutKeyboardFunc( keyboard );						// register Keyboard Handler
-	initialize();
-	glutMainLoop();							// run GLUT mainloop
-	return 0;
+	mainInit();
+	viewInit();
+	bottomInit();
+	mapInit();
+	glutMainLoop();												// run GLUT mainloop
 }
