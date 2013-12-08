40f31da125dd3ab3c53be7cfd7884d3d74535c78
Kyle Falconer <kfalconer@gmail.com>
2013-12-08 15:22:44 -0600

    Shorted the status for the second floor so that it would fit on one line.

--- project/Project/Project/states.h
+++ project/Project/Project/states.h
@@ -12,7 +12,7 @@ const std::string transitionlabels[NUM_STATES][3] = {
 	{"You see a receptionist, who suggests that you speak to H.R.", "1 - Talk to Human Resources.", "2 - Ignore the receptionist and climb out the window."},	// secretary
 	{"The Human Resources lady looks angry.", "1 - Talk to her.", "2 - Climb out the window."},	// HR
 	{"You poke your head out the window and see a ladder.", "1 - Climb up to the second floor.","2 - Climb down to the parking lot."},	// Window
-	{"You are on the second floor and see a corridor with labels at various entrance ways.","1 - Visit the food court.", "2 - Go to the stair well."}, // 2nd floor
+	{"You are on the second floor and see a corridor with labeled entrances.","1 - Visit the food court.", "2 - Go to the stair well."}, // 2nd floor
 	{"You are now at the food court and see the stair well again in the distance.","1 - Eat some food.", "2 - Go to the stair well."}, // food court
 	{"You are standing in the stair well.","1 - Go up the stairs.", "2 - Go down the stairs."},
 	{"You are now on the third floor, back on familiar ground.","1 - Go to your desk.", "2 - Go back down the stairs."},
